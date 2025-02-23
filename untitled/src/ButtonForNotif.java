57ca4749429340f989e5269f4b3530611c845386
Jailyn <jam2257@miami.edu>
2025-02-23 11:54:54 -0500

    feature/notifications add notifications to the application

---
+++ untitled/src/ButtonForNotif.java
@@ -0,0 +1,17 @@
+public class ButtonForNotif {
+    private boolean isChecked;
+
+    public ButtonForNotif(boolean isChecked) {
+        this.isChecked = isChecked;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+
+}
